07c6d0f15a63e88e6c0e6cdffd4eec74f8c2ec81
parkervg <parkervg5@gmail.com>
2020-11-06 08:39:06 -0800

    tools/ranking, evaluate function on WE class

--- lib/ProcessEmbeddings.py
+++ lib/ProcessEmbeddings.py
@@ -9,6 +9,9 @@ import imp
 import os
 import uuid
 import sys
+import math
+from tabulate import tabulate
+from tools.ranking import *
 EPSILON = 1e-6
 RAND_STATE = 324
 logger = Blogger()
@@ -16,6 +19,7 @@ logger = Blogger()
 # Set PATHs
 PATH_TO_SENTEVAL = './SentEval'
 PATH_TO_DATA = './SentEval/data'
+WORD_SIM_DIR = './data/word-sim'
 
 # import SentEval
 sys.path.insert(0, PATH_TO_SENTEVAL)
@@ -98,7 +102,15 @@ class WordEmbeddings:
                           learning_rate=flags_file.FLAGS.learning_rate)
             fcwta.saver.restore(sess, checkpoint_path)
             self.embeds = fcwta.encode(sess, self.embeds)
+        tf.reset_default_graph()
+        self._del_all_flags(flags_file.FLAGS) # So next run won't raise error
 
+    @staticmethod
+    def _del_all_flags(FLAGS):
+        flags_dict = FLAGS._flags()
+        keys_list = [keys for keys in flags_dict]
+        for keys in keys_list:
+            FLAGS.__delattr__(keys)
 
     # Create dictionary
     def _create_dictionary(self, sentences, threshold=0):
@@ -163,32 +175,81 @@ class WordEmbeddings:
         return embeddings
 
 
-    def SentEval(self, tasks, save_summary=False, summary_file_name=None):
+    def similarity_tasks(self, save_summary=False, summary_file_name=None):
+        self.summary["similarity_scores"] = {}
+        word_vecs = {word:vector for word, vector in zip(self.ordered_vocab, self.embeds)}
+        # Normalize for similarity tasks
+        # Levy et. al. 2015
+        #   "Vectors are normalized to unit length before they are used for similarity calculation,
+        #    making cosine similarity and dot-product equivalent.""
+        word_vecs = {word:vector/math.sqrt((vector**2).sum() + EPSILON) for word, vector in word_vecs.items()}
+        table = []
+        for i, filename in enumerate(os.listdir(WORD_SIM_DIR)):
+            manual_dict, auto_dict = ({}, {})
+            not_found, total_size = (0, 0)
+            for line in open(os.path.join(WORD_SIM_DIR, filename),'r'):
+                line = line.strip().lower()
+                word1, word2, val = line.split()
+                if word1 in word_vecs and word2 in word_vecs:
+                    manual_dict[(word1, word2)] = float(val)
+                    auto_dict[(word1, word2)] = cosine_sim(word_vecs[word1], word_vecs[word2])
+                else:
+                    not_found += 1
+                total_size += 1
+            rho = spearmans_rho(assign_ranks(manual_dict), assign_ranks(auto_dict))
+            self.summary["similarity_scores"][filename] = rho
+            table.append([filename, total_size, not_found, rho])
+        print(tabulate(table, headers=["Dataset", "Num Pairs", "Not Found", "Rho"]))
+
+    def evaluate(self, senteval_tasks, save_summary=False, summary_file_name=None, senteval_config={}):
+        """
+        Runs SentEval classification tasks, and similarity tasks from Half-Size.
+        """
+        self.summary = {}
+        self.SentEval(senteval_tasks, save_summary=save_summary, summary_file_name=summary_file_name, senteval_config=senteval_config)
+        self.similarity_tasks(save_summary=save_summary, summary_file_name=summary_file_name)
+        self.summary["original_dim"] = self.original_dim
+        self.summary["final_dim"] = self.embeds.shape[1]
+        self.summary["process"] = self.function_log
+        summary_file_name = summary_file_name if summary_file_name else str(uuid.uuid4())
+        self.save_summary_json(summary_file_name)
+
+    def SentEval(self, tasks, save_summary=False, summary_file_name=None, senteval_config={}):
+        # Set params for SentEval
+        params_senteval = {'task_path': PATH_TO_DATA,
+                           'usepytorch': senteval_config.get("usepytorch") if senteval_config.get("usepytorch") else False,
+                           'kfold': senteval_config.get("kfold") if senteval_config.get("kfold") else 5}
+        params_senteval['classifier'] = {'nhid': senteval_config.get("nhid") if senteval_config.get("nhid") else 0,
+                                         'optim': senteval_config.get("optim") if senteval_config.get("optim") else 'rmsprop',
+                                         'batch_size': senteval_config.get("batch_size") if senteval_config.get("batch_size") else 128,
+                                         'tenacity': senteval_config.get("tenacity") if senteval_config.get("tenacity") else 3,
+                                         'epoch_size': senteval_config.get("epoch_size") if senteval_config.get("epoch_size") else 2,}
+        se = senteval.engine.SE(params_senteval, self.batcher, self.prepare)
+        self.summary["classification_scores"] = {}
+        results = se.eval(tasks)
+        for k in results:
+            self.summary["classification_scores"][k] = results[k]["acc"]
+            logger.status_update("{}: {}".format(k, results[k]["acc"]))
+            print()
+
+    def model_inference(self, task):
+        """
+        Returns the classfier and X, Y data used in SentEval task
+        """
         # Set params for SentEval
         params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': False, 'kfold': 5}
         params_senteval['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
                                          'tenacity': 3, 'epoch_size': 2}
         se = senteval.engine.SE(params_senteval, self.batcher, self.prepare)
-        results = se.eval(tasks)
-        for k in results:
-            summary["scores"][k] = results[k]["acc"]
-            logger.status_update("{}: {}".format(k, results[k]["acc"]))
-            print()
-        if save_summary:
-            summary = {}
-            summary["original_dim"] = self.original_dim
-            summary["final_dim"] = self.embeds.shape[1]
-            summary["process"] = self.function_log
-            summary["scores"] = {}
-            summary_file_name = summary_file_name if summary_file_name else str(uuid.uuid4())
-            self.save_summary_json(summary, summary_file_name)
+        results = se.eval(task)
+        return results["acc"], results["classifier"], results["X"], results["Y"]
 
-    @staticmethod
-    def save_summary_json(summary, summary_file_name):
+
+    def save_summary_json(self, summary_file_name):
         if not os.path.isdir("summary"):
             os.mkdir("summary")
         with open("summary/{}".format(summary_file_name), 'w') as f:
-            json.dump(summary, f)
+            json.dump(self.summary, f)
         logger.status_update("Summary saved to summary/{}".format(summary_file_name))
 
     def save_vectors(self, output_file):
